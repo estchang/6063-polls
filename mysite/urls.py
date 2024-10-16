a601322dab41ec6c6d1611ef19b436cd289e1677
estcha <chang.estella@gmail.com>
2024-10-15 22:08:56 -0400

    change travis to deploy on pr
    remove pycache

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+	path("", lambda request: redirect("polls/")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
